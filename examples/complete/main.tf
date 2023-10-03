9f526704cb631ab2eaf2a32562d65f46038ee88a
Patrick Mukumbu <mukumbu.katiwa@boldlink.io>
2023-10-03 16:07:17 +0300

    Added usage of multi_region key in complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,10 +1,28 @@
+provider "aws" {
+  region = "eu-west-2"
+  alias  = "replica"
+}
 
 module "complete_kms" {
   source                  = "./../../"
   description             = "Example complete kms key with IAM user permisions"
-  alias_name              = "alias/example-key-alias"
-  enable_key_rotation     = true
+  alias_name              = "alias/${var.name}"
   create_kms_alias        = true
+  multi_region            = true
   kms_policy              = local.policy
-  deletion_window_in_days = 7
+  deletion_window_in_days = var.deletion_window_in_days
+  tags                    = var.tags
+}
+
+resource "aws_kms_replica_key" "replica" {
+  provider                = aws.replica
+  description             = "regional replica key for ${var.name}"
+  deletion_window_in_days = var.deletion_window_in_days
+  primary_key_arn         = module.complete_kms.arn
+}
+
+resource "aws_kms_alias" "replica" {
+  provider      = aws.replica
+  name          = "alias/${var.name}-replica"
+  target_key_id = aws_kms_replica_key.replica.key_id
 }
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,1 +1,26 @@
-#(empty)
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to the kms resources"
+  default = {
+    Environment        = "example"
+    "user::CostCenter" = "terraform-registry"
+    Department         = "DevOps"
+    Project            = "Examples"
+    InstanceScheduler  = true
+    Owner              = "Boldlink"
+    LayerName          = "cExample"
+    LayerId            = "cExample"
+  }
+}
+
+variable "name" {
+  type        = string
+  description = "Name to be used in creating kms key alias"
+  default     = "complete-kms-example"
+}
+
+variable "deletion_window_in_days" {
+  description = " The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key. If you specify a value, it must be between 7 and 30, inclusive. If you do not specify a value, it defaults to 30. If the KMS key is a multi-Region primary key with replicas, the waiting period begins when the last of its replica keys is deleted. Otherwise, the waiting period begins immediately."
+  type        = number
+  default     = 7
+}
